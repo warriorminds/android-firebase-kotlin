99f9be58de26aab5d7ac14e4de01cfedcd7f40e6
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-01-29 15:45:49 -0600

    Creación actividad principal y pantalla para iniciar sesión y opciones de sesión.

---
+++ app/src/main/java/com/warriorminds/firebasekotlin/autenticacion/ActividadAutenticacionCorreo.kt
@@ -0,0 +1,13 @@
+package com.warriorminds.firebasekotlin.autenticacion
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.warriorminds.firebasekotlin.R
+
+class ActividadAutenticacionCorreo : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actividad_autenticacion_correo)
+    }
+}
