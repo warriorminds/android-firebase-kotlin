bece7c15f6b7b118ee770d2dafb2395131450463
Rodrigo Guerrero <rodrigo.x.guerero.-ND@disney.com>
2018-03-01 17:04:04 -0600

    Inicializando AdMob

--- app/src/main/java/com/warriorminds/firebasekotlin/Aplicacion.kt
+++ app/src/main/java/com/warriorminds/firebasekotlin/Aplicacion.kt
@@ -1,6 +1,7 @@
 package com.warriorminds.firebasekotlin
 
 import android.app.Application
+import com.google.android.gms.ads.MobileAds
 import com.twitter.sdk.android.core.Twitter
 
 /**
@@ -11,5 +12,6 @@ class Aplicacion : Application() {
     override fun onCreate() {
         super.onCreate()
         Twitter.initialize(this)
+        MobileAds.initialize(this, "ca-app-pub-7034494771274338~8545650534")
     }
 }
